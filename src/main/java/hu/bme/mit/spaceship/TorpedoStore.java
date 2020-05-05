591448eadc10777165184c48ff3ca238c178f492
nandiMstr <lengyel.nandi@gmail.com>
2020-05-05 11:22:14 +0200

    Fix Exception throwing

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,7 +30,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("x must be nonnegative");
     }
 
     boolean success = false;
