32019ab466332dccf672233b528cd1222859efb5
nandiMstr <lengyel.nandi@gmail.com>
2020-05-08 10:31:43 +0200

    Fix random number generating

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -37,7 +37,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    double r = this.rand.nextInt();
+    double r = this.rand.nextDouble();
 
     if (r >= FAILURE_RATE) {
       // successful firing
