87703ffb0605999b0c9bc3323d41b1bf14d0a20f
nandiMstr <lengyel.nandi@gmail.com>
2020-05-08 10:28:41 +0200

    Change Exception message

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -31,7 +31,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("x must be nonnegative");
+      throw new IllegalArgumentException("numberOfTorpedos must be nonnegative");
     }
 
     boolean success = false;
